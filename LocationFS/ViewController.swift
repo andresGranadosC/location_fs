26f2bfac8b4346a6fd29fa1cffea9d95e102e599
Andres Granados <andresgranados@Andress-MacBook-Pro.local>
2019-06-11 17:32:08 -0500

    View Created

--- LocationFS/ViewController.swift
+++ LocationFS/ViewController.swift
@@ -12,19 +12,58 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var venuesTableView: UITableView!
     
+    var responseRequest: FSResponseRequest?
+    var venues: [Venue] = [] {
+        didSet {
+            self.venuesTableView.delegate = self
+            self.venuesTableView.dataSource = self
+            self.venuesTableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ServicesInteractor.requestVenues { (responseRequest, error) in
-            
-            
+            self.responseRequest = responseRequest
+            self.loadVenues()
         }
         
     }
     
+    func loadVenues() {
+        
+        if let responseRequest = responseRequest, responseRequest.response.groups.count > 0, let group = responseRequest.response.groups.first  {
+            
+            let venues = group.items.map { (item) -> Venue in
+                
+                return item.venue
+                }
+            
+            self.venues = venues
+        }
+    }
     
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.venues.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "VenueCell") as? VenueTableViewCell
+        
+        cell?.textLabel?.text = venues[indexPath.row].name
+        cell?.detailTextLabel?.text = "\(venues[indexPath.row].location.lat) \(venues[indexPath.row].location.lng)"
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    
 }
 
